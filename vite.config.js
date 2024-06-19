e0a0d4d0f6ab7153828726a08d4bc8c71e7a0211
mitsuxhoney <hiveorxythm@gmail.com>
2024-06-19 17:25:42 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
+
+// https://vitejs.dev/config/
+
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/mern-pro1-intern/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
